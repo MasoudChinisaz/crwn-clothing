1130bc8196f769ed55f36501f0ddbb982f4671f5
Masoud Chinisaz <masoud.chinisaz@yahoo.com>
2022-04-19 16:19:30 +0430

    update our menu-items to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,9 @@ import Directory from "../../components/directory/directory.component";
 
 import './homepage.styles.scss'
 
+
+
+
 const HomePage = () => (
   <div className='homepage'>
     <Directory />
